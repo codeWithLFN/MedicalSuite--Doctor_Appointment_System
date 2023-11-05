a85ea1d41b25588f7654f1eada22f43936f60b12
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2023-11-05 23:07:17 +0200

    update

---
+++ Website/MedicalSuite/firebase/firebase-config.js
@@ -0,0 +1,8 @@
+{
+    apiKey: "AIzaSyBVmFA1iK5MSKLIhf3ACo6E0meUeRbPCgs";
+    authDomain: "medicalsuite-99e48.firebaseapp.com";
+    projectId: "medicalsuite-99e48";
+    storageBucket: "medicalsuite-99e48.appspot.com";
+    messagingSenderId: "679686285511";
+    appId: "1:679686285511:web:be7e727760a15a61a9f6e2"
+}
